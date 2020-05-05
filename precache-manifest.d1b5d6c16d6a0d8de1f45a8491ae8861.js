c5ecfab451941a567a4ecf6ca08ba9a93011288d
Betsy Mendoza Gomez <betsai.mendozagomez@gmail.com>
2020-05-04 23:20:33 -0500

    Updates

---
+++ precache-manifest.d1b5d6c16d6a0d8de1f45a8491ae8861.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e024c6d01afee3ca0e5faa0e73a25a",
+    "url": "/mirall/index.html"
+  },
+  {
+    "revision": "1dd174686cca2ee8e2d4",
+    "url": "/mirall/static/css/main.eb931041.chunk.css"
+  },
+  {
+    "revision": "de53360d4ded5bae5d82",
+    "url": "/mirall/static/js/2.edf49313.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mirall/static/js/2.edf49313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dd174686cca2ee8e2d4",
+    "url": "/mirall/static/js/main.175c7729.chunk.js"
+  },
+  {
+    "revision": "9642aa8ffe3ab73ff107",
+    "url": "/mirall/static/js/runtime-main.7f36e73b.js"
+  },
+  {
+    "revision": "b3f451aab275d80dd7e8588aa8853ad0",
+    "url": "/mirall/static/media/artIcon.b3f451aa.svg"
+  },
+  {
+    "revision": "85876ba22d6968b378cd2bce11114c0c",
+    "url": "/mirall/static/media/bigCircle.85876ba2.svg"
+  },
+  {
+    "revision": "d5961ad126afdaa120f4234834e589bb",
+    "url": "/mirall/static/media/bigTriangle.d5961ad1.svg"
+  },
+  {
+    "revision": "fe43d6a504f5c83a0696031460db951b",
+    "url": "/mirall/static/media/designIcon.fe43d6a5.svg"
+  },
+  {
+    "revision": "594694daede651b6a8ee98a752643470",
+    "url": "/mirall/static/media/dotGrid.594694da.svg"
+  },
+  {
+    "revision": "92c9970b81084c3a02305810019fd666",
+    "url": "/mirall/static/media/entrepreneurshipIcon.92c9970b.svg"
+  },
+  {
+    "revision": "acb1942ba51c0dabcb403838647bb96a",
+    "url": "/mirall/static/media/fitnessIcon.acb1942b.svg"
+  },
+  {
+    "revision": "d9d6bee5e56ea66662d9a3ee1cdbf934",
+    "url": "/mirall/static/media/homeBackground.d9d6bee5.png"
+  },
+  {
+    "revision": "39e976e03c275556696887685d09c4c2",
+    "url": "/mirall/static/media/kitchenIcon.39e976e0.svg"
+  },
+  {
+    "revision": "73beeff87f115ca135a8fa6afa1e59c6",
+    "url": "/mirall/static/media/legalIcon.73beeff8.svg"
+  },
+  {
+    "revision": "5913ccf422b95a4126fb0f7be6cac6bd",
+    "url": "/mirall/static/media/lenguageIcon.5913ccf4.svg"
+  },
+  {
+    "revision": "cffa7ecab126a55524408764177b4bf2",
+    "url": "/mirall/static/media/marketingIcon.cffa7eca.svg"
+  },
+  {
+    "revision": "eb8651c1eda679883f19b62ea9c5e4bf",
+    "url": "/mirall/static/media/middleTriangle.eb8651c1.svg"
+  },
+  {
+    "revision": "09b6af36e2ff3c9560d12a70afd65aad",
+    "url": "/mirall/static/media/serviceSale.09b6af36.png"
+  },
+  {
+    "revision": "0b1939e39ea9f270380997ca2719c3a2",
+    "url": "/mirall/static/media/triangle.0b1939e3.svg"
+  }
+]);
